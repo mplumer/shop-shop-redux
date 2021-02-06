ac449c380ad6ec80d304c2556363ecf52efc4a43
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-02-06 15:04:05 -0500

    set up store, actions, reducers

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,11 +1,55 @@
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
-
-export const ADD_TO_CART = "ADD_TO_CART";
-export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const CLEAR_CART = "CLEAR_CART";
-export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
-export const TOGGLE_CART = "TOGGLE_CART";
-
 export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
+
+export const updateProducts = (products) => ({
+    type: UPDATE_PRODUCTS,
+    products,
+  });
+  
+  export const updateCategories = (categories) => ({
+    type: UPDATE_CATEGORIES,
+    categories,
+  });
+  
+  export const updateCurrentCategory = (currentCategory) => ({
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory,
+  });
+  
+  export const addToCart = (item) => ({
+    type: ADD_TO_CART,
+    product: { ...item, purchaseQuantity: 1 },
+  });
+  
+  export const addMultipleToCart = (cart) => ({
+    type: ADD_MULTIPLE_TO_CART,
+    products: [...cart],
+  });
+  
+  export const removeFromCart = (_id) => ({
+    type: REMOVE_FROM_CART,
+    _id,
+  });
+  
+  export const updateCartQuantity = (_id, purchaseQuantity) => ({
+    type: UPDATE_CART_QUANTITY,
+    _id,
+    purchaseQuantity,
+  });
+  
+  export const clearCart = (cart) => ({
+    type: CLEAR_CART,
+    products: [...cart],
+  });
+  
+  export const toggleCart = (cart) => ({
+    type: TOGGLE_CART,
+    cart,
+  });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,90 +1,97 @@
-import { useReducer } from "react";
 import {
   UPDATE_PRODUCTS,
-  ADD_TO_CART,
-  UPDATE_CART_QUANTITY,
-  REMOVE_FROM_CART,
-  ADD_MULTIPLE_TO_CART,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
   CLEAR_CART,
-  TOGGLE_CART
+  TOGGLE_CART,
 } from "./actions";
 
-export const reducer = (state, action) => {
+// import { useReducer } from "react";
+const initialState = {
+  categories: [],
+  currentCategory: "",
+  products: [],
+  cart: [],
+  cartOpen: false
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
     case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
     case ADD_TO_CART:
       return {
         ...state,
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
-
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-
-    case UPDATE_CART_QUANTITY:
-      return {
-        ...state,
-        cartOpen: true,
-        cart: state.cart.map(product => {
-          if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity
-          }
-          return product
-        })
-      };
-
     case REMOVE_FROM_CART:
-      let newState = state.cart.filter(product => {
+      let newState = state.cart.filter((product) => {
         return product._id !== action._id;
       });
 
       return {
         ...state,
         cartOpen: newState.length > 0,
-        cart: newState
+        cart: newState,
       };
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            return { ...product, purchaseQuantity: action.purchaseQuantity };
+          }
+          return product;
+          // Why did we need to use the map() method to create a new array instead of updating state.cart directly?
 
+          // Answer: The original state should be treated as immutable.
+        }),
+      };
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
-        cart: []
+        cart: [],
       };
-
     case TOGGLE_CART:
       return {
         ...state,
-        cartOpen: !state.cartOpen
+        cartOpen: !state.cartOpen,
       };
-
-    case UPDATE_CATEGORIES:
-      return {
-        ...state,
-        categories: [...action.categories],
-      };
-
-    case UPDATE_CURRENT_CATEGORY:
-      return {
-        ...state,
-        currentCategory: action.currentCategory
-      }
 
     default:
       return state;
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState)
-}
+// export function useProductReducer(initialState) {
+//   return useReducer(reducer, initialState);
+// }
+
+export default reducer
